bea4aa5baa36ba89ffd9ec5202809283bdaefbd9
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-03-02 22:21:40 +0300

    Поправил тесты

--- lab1/Replace/test.bat
+++ lab1/Replace/test.bat
@@ -15,24 +15,25 @@ fc.exe "%TEMP%\replace-lion.txt" "replace-lion.txt"
 if ERRORLEVEL 1 goto err
 
 rem замена существующей подстроки
-rem %PROGRAM% fox.txt %TEMP%\fox.txt dog cat
-rem if ERRORLEVEL 1 goto err
-rem fc.exe  %TEMP%\fox.txt fox.txt
-rem if ERRORLEVEL 1 goto err
+%PROGRAM% fox.txt %TEMP%\fox.txt dog cat
+if ERRORLEVEL 1 goto err
+fc.exe  %TEMP%\fox.txt fox.txt
+if ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается нулевой код возврата
-rem %PROGRAM% fox-dog-with-cat.txt %TEMP%\fox-dog-with-cat.txt fox cat
-rem if ERRORLEVEL 1 goto err
-rem fc.exe  %TEMP%\fox-dog-with-cat.txt fox-dog-with-cat.txt
-rem if ERRORLEVEL 1 goto err
+%PROGRAM% fox-dog-with-cat.txt %TEMP%\fox-dog-with-cat.txt fox cat
+if ERRORLEVEL 1 goto err
+fc.exe  %TEMP%\fox-dog-with-cat.txt fox-dog-with-cat.txt
+if ERRORLEVEL 1 goto err
 
-rem копирование пустого файла
+rem копирование с заменой пустого файла
 %PROGRAM% empty.txt %TEMP%\empty.txt fox cat
 if ERRORLEVEL 1 goto err
 fc.exe %TEMP%\empty.txt empty.txt
 if ERRORLEVEL 1 goto err
 
-%PROGRAM% fox-witout-param.txt %TEMP%\fox-witout-param.txt fox cat
+rem копирование с заменой без параметров
+%PROGRAM% fox-witout-param.txt %TEMP%\fox-witout-param.txt
 if ERRORLEVEL 1 goto err
 fc.exe  %TEMP%\fox-witout-param.txt fox-witout-param.txt
 if ERRORLEVEL 1 goto err
