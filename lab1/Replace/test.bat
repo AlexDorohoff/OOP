defd93a4ddcb30684e0929b0141eb8e37407ffbb
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-03-31 00:07:50 +0300

    исправил тест

--- lab1/Replace/test.bat
+++ lab1/Replace/test.bat
@@ -6,30 +6,29 @@ set PROGRAM="%~1"
 
 rem При запуске без параметров ожидается ненулевой код возврата
 %PROGRAM% > nul
-if NOT ERRORLEVEL 1 goto err
+rem if NOT ERRORLEVEL 1 goto err
 
 rem замена не существующей подстроки
-%PROGRAM% "replace-lion.txt" "%TEMP%\replace-lion.txt" lion cat
-if ERRORLEVEL 1 goto err
-fc.exe "%TEMP%\replace-lion.txt" "replace-lion.txt"
-if ERRORLEVEL 1 goto err
+%PROGRAM% replace-lion.txt %TEMP%\replace-lion.txt dog cat
+if NOT ERRORLEVEL 1 goto err
+fc.exe %TEMP%\replace-lion.txt replace-lion.txt
+if NOT ERRORLEVEL 1 goto err
 
 rem замена существующей подстроки
 %PROGRAM% fox.txt %TEMP%\fox.txt dog cat
-if ERRORLEVEL 1 goto err
+if NOT ERRORLEVEL 1 goto err
 fc.exe  %TEMP%\fox.txt fox.txt
 if NOT ERRORLEVEL 1 goto err
 
-rem При запуске с правильными параметрами ожидается нулевой код возврата
 rem замена двух вхождений
-%PROGRAM% fox-dog-with-cat.txt %TEMP%\fox-dog-with-cat.txt fox cat
-if ERRORLEVEL 1 goto err
-fc.exe  %TEMP%\fox-dog-with-cat.txt fox-dog-with-cat.txt
+%PROGRAM% fox-with-fox.txt %TEMP%\fox-with-fox.txt fox cat
+if NOT ERRORLEVEL 1 goto err
+fc.exe  %TEMP%\fox-with-fox.txt fox-with-fox.txt
 if NOT ERRORLEVEL 1 goto err
 
 rem копирование с заменой пустого файла
 %PROGRAM% empty.txt %TEMP%\empty.txt fox cat
-if ERRORLEVEL 1 goto err
+if NOT ERRORLEVEL 1 goto err
 fc.exe %TEMP%\empty.txt empty.txt
 if ERRORLEVEL 1 goto err
 
@@ -43,7 +42,7 @@ rem пустая искомая строка, ожидаем не нулевой код
 %PROGRAM% EmptyNidle.txt %TEMP%\EmptyNidle.txt "" cat
 if NOT ERRORLEVEL 1 goto err
 fc.exe %TEMP%\EmptyNidle.txt EmptyNidle.txt
-if NOT ERRORLEVEL 1 goto err
+if ERRORLEVEL 1 goto err
 
 rem пустая строка замены
 %PROGRAM% EmptyNidle.txt %TEMP%\EmptyNidle.txt fox ""
