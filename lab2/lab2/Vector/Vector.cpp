37c46459e1517c1e0b84a26d77fdbd35414e6e69
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-04-06 21:59:37 +0300

    обработка пустого вектора

--- lab2/lab2/Vector/Vector.cpp
+++ lab2/lab2/Vector/Vector.cpp
@@ -8,14 +8,22 @@ void GetVector(std::istream& input, std::vector<double>& numbers)
 	std::copy(std::istream_iterator<double>(input), std::istream_iterator<double>(), std::back_inserter(numbers));
 }
 
-void ProcessVector(std::vector<double>& numbers)
+bool ProcessVector(std::vector<double>& numbers)
 {
+	if (numbers.empty())
+	{
+		return false;
+	}
 	const double min = *std::min_element(numbers.begin(), numbers.end());
 	std::transform(numbers.begin(), numbers.end(), numbers.begin(), [=](double number) { return number = number * min; });
+}
+void SortVector(std::vector<double>& numbers)
+{
+	std::sort(numbers.begin(), numbers.end());
 }
 
 void PrintVector(std::vector<double>& numbers)
 {
-	std::cout << "printVector";
+	std::cout << "print new Vector: ";
 	copy(numbers.begin(), numbers.end(), std::ostream_iterator<double>(std::cout, " "));
 }
--- lab2/lab2/Vector/vector.h
+++ lab2/lab2/Vector/vector.h
@@ -1,5 +1,6 @@
 #pragma once
 
 void GetVector(std::istream& input, std::vector<double>& numbers);
-void ProcessVector(std::vector<double>& numbers);
+bool ProcessVector(std::vector<double>& numbers);
+void SortVector(std::vector<double>& numbers);
 void PrintVector(std::vector<double>& numbers);
