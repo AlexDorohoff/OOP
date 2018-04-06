69f0e80019154162602fcde74fc204a826868156
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-04-06 22:27:58 +0300

    #4
    -Изменил ProcessVector с bool на void
    -Назвал тесты

--- lab2/lab2/Vector/Vector.cpp
+++ lab2/lab2/Vector/Vector.cpp
@@ -8,14 +8,13 @@ void GetVector(std::istream& input, std::vector<double>& numbers)
 	std::copy(std::istream_iterator<double>(input), std::istream_iterator<double>(), std::back_inserter(numbers));
 }
 
-bool ProcessVector(std::vector<double>& numbers)
+void ProcessVector(std::vector<double>& numbers)
 {
-	if (numbers.empty())
+	if (!(numbers.empty()))
 	{
-		return false;
+		const double min = *std::min_element(numbers.begin(), numbers.end());
+		std::transform(numbers.begin(), numbers.end(), numbers.begin(), [=](double number) { return number = number * min; });
 	}
-	const double min = *std::min_element(numbers.begin(), numbers.end());
-	std::transform(numbers.begin(), numbers.end(), numbers.begin(), [=](double number) { return number = number * min; });
 }
 void SortVector(std::vector<double>& numbers)
 {
