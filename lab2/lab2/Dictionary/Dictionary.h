61d841923639dcf73b5bbc508dbf81367b0b82e2
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-04-28 01:01:11 +0300

    Разобраться с чтением и довавлением всего словаря в

--- lab2/lab2/Dictionary/Dictionary.h
+++ lab2/lab2/Dictionary/Dictionary.h
@@ -1,7 +1,4 @@
 #pragma once
-#include <map>
-
-typedef std::map<std::string, std::string> Dictionary;
 
 std::string Translate(const std::string& inputWord, const std::string& inputFileName);
 std::ifstream OpenFile(const std::string& inputFileName)
