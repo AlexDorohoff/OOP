d81d8fd3ec303972dc789cf1cbe7616a791a084e
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-06-15 23:10:36 +0300

    Time

---
+++ lab5/lab5/CTime.cpp
@@ -0,0 +1,116 @@
+#include "stdafx.h"
+#include "CTime.h"
+
+constexpr unsigned SECONDS_IN_HOUR = 3600;
+constexpr unsigned SECONDS_IN_MINUTE = 60;
+constexpr unsigned SECONDS_IN_DAY = 86400;
+
+constexpr unsigned HOURS_IN_DAY = 24;
+constexpr unsigned MINUTES_IN_HOUR = 60;
+
+CTime::CTime(unsigned hours, unsigned minutes, unsigned seconds)
+{
+	if (hours > HOURS_IN_DAY || minutes > MINUTES_IN_HOUR || seconds > SECONDS_IN_MINUTE)
+	{
+		throw std::invalid_argument("invalid argument \n");
+	}
+	m_timestamp = SECONDS_IN_HOUR * hours + SECONDS_IN_MINUTE * minutes + seconds;
+	std::cout << "timestamp : " << m_timestamp << "end\n";
+}
+
+unsigned CTime::GetHours() const
+{
+	return m_timestamp / SECONDS_IN_HOUR;
+}
+
+unsigned CTime::GetMinutes() const
+{
+	return m_timestamp / SECONDS_IN_MINUTE - (m_timestamp / SECONDS_IN_HOUR) * SECONDS_IN_MINUTE;
+}
+
+unsigned CTime::GetSeconds() const
+{
+	return m_timestamp % SECONDS_IN_MINUTE;
+};
+
+CTime::CTime(unsigned timeStamp)
+{
+	if (timeStamp >= SECONDS_IN_DAY)
+	{
+		throw std::invalid_argument("invalid argument 2\n");
+	}
+	m_timestamp = timeStamp;
+}
+
+unsigned CTime::GetM_Time() const
+{
+	return m_timestamp;
+}
+
+CTime& CTime::operator++()
+{
+	if (m_timestamp >= SECONDS_IN_DAY)
+	{
+		m_timestamp = m_timestamp - SECONDS_IN_DAY;
+	}
+	else
+	{
+		++m_timestamp;
+	}
+	return *this;
+}
+
+CTime const CTime::operator++(int)
+{
+	// создаем копию, выполняем предынкремент и возвращаем копию
+	//CCounter tmpCopy(*this);
+	//++*this;
+	//return tmpCopy;
+	unsigned tmpTime = m_timestamp;
+	if (tmpTime >= SECONDS_IN_DAY)
+	{
+		tmpTime = tmpTime - SECONDS_IN_DAY;
+	}
+	else
+	{
+		m_timestamp++;
+	}
+	return tmpTime;
+}
+
+CTime& CTime::operator--()
+{
+	if (m_timestamp == 0)
+	{
+		m_timestamp = SECONDS_IN_DAY - 1;
+	}
+	else
+	{
+		--m_timestamp;
+	}
+	return *this;
+}
+
+CTime const CTime::operator--(int)
+{
+	// создаем копию, выполняем предынкремент и возвращаем копию
+	//CCounter tmpCopy(*this);
+	//++*this;
+	//return tmpCopy;
+	unsigned tmpTime = m_timestamp;
+	if (m_timestamp == 0)
+	{
+		tmpTime = SECONDS_IN_DAY - 1;
+	}
+	else
+	{
+		--m_timestamp;
+	}
+	return tmpTime;
+}
+
+CTime& CTime::operator+(CTime const& time2)
+{
+	CTime time3;
+	unsigned sum = m_timestamp + time2.GetSeconds();
+	return time3(sum);
---
+++ lab5/lab5/CTime.h
@@ -0,0 +1,42 @@
+#pragma once
+// секунд (0-59)
+
+class CTime
+
+{
+
+public:
+	// инициализирует время заданным количеством часов, минут и секунд
+
+	CTime(unsigned hours, unsigned minutes, unsigned seconds = 0);
+
+	// инициализирует время количеством секунд после полуночи
+
+	CTime(unsigned timeStamp = 0);
+
+	// возвращает количество часов
+
+	unsigned GetHours() const;
+
+	// возвращает количество минут
+
+	unsigned GetMinutes() const;
+
+	// возвращает количество секунд
+
+	unsigned GetSeconds() const;
+
+	unsigned GetM_Time() const;
+
+	CTime& operator++();
+	CTime const operator++(int);
+
+	CTime& operator--();
+	CTime const operator--(int);
+
+	CTime& operator+(CTime const& time2);
+	CTime& operator-();
+
+private:
+	unsigned m_timestamp = 0;
+};
