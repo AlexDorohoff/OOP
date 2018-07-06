86f43591c6d21a614ea4e98977eea8a8a8e4ce6b
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-07-06 15:53:56 +0300

    набросок

--- lab7/ConsoleApplication1/FindMaxEx/FindMaxEx.h
+++ lab7/ConsoleApplication1/FindMaxEx/FindMaxEx.h
@@ -3,4 +3,16 @@ template <typename T, typename Less>
 
 bool FindMax(std::vector<T> const& arr, T& maxValue, Less const& less)
 {
+	if (arr.empty())
+	{
+		return false;
+	}
+
+	size_t size = arr.size();
+	for (size_t i = 0; i < size; i++)
+	{
+		*maxValue = less(arr[i], *maxValue);
+	}
+
+	return *maxValue;
 }
